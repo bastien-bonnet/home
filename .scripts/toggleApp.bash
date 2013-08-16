39750455d408f3ebb2ddaf9d1a1a1e79c0dec9cc
bbonnet <none>
2013-08-16 20:06:15 +0200

    started geometry work

--- .scripts/toggleApp.bash
+++ .scripts/toggleApp.bash
@@ -6,19 +6,25 @@ VISIBLE_W_ID=$(xdotool search --onlyvisible --classname $SEARCHED_WINDOW 2>/dev/
 
 if [ -z "$W_ID" ]; then
 	echo "NO WINDOW FOUNT"
-  $SEARCHED_WINDOW
+	$SEARCHED_WINDOW
 	NEW_W_ID=$(wmctrl -lx | grep -i $SEARCHED_WINDOW | head -n 1 | cut -d ' ' -f1)
-  xdotool windowfocus $NEW_W_ID
+	xdotool windowfocus $NEW_W_ID
 elif [ -z "$VISIBLE_W_ID" ]; then
 	echo "UNMAPPED WINDOW FOUND"
 	for wid in $W_ID; do
-		
 		if [[ !($(xprop -id $wid WM_WINDOW_ROLE) =~ .*not\ found.* )]]; then
-  		xdotool windowmap $wid
-  		xdotool windowfocus $wid
+			xdotool windowmap $wid
+			xdotool windowfocus $wid
 		fi
 	done
 else
 	echo "VISIBLE WINDOW FOUND"
-  xdotool windowunmap $VISIBLE_W_ID
+	windowPosition=$(xdotool getwindowgeometry $VISIBLE_W_ID | grep Position | sed "s/^.* \([0-9]*,[0-9]*\).*$/\1/")
+	windowX=$(echo $windowPosition | cut -d ',' -f1)
+	windowY=$(echo $windowPosition | cut -d ',' -f2)
+	echo $windowX $windowY
+
+	xdotool windowunmap --sync $VISIBLE_W_ID
+	sleep 0.2s
+	xdotool windowmove $VISIBLE_W_ID $windowX $windowY
 fi
