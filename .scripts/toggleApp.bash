f49cad8f4640bdb56f1632a6c41db65189524a31
Bastien <bast.bonnet@laposte.net>
2013-08-17 03:16:56 +0200

    Now using pid to identify window, cleaned debug instructions

--- .scripts/toggleApp.bash
+++ .scripts/toggleApp.bash
@@ -1,30 +1,20 @@
 #!/bin/bash
 
-SEARCHED_WINDOW=dolphin
-W_ID=$(xdotool search --classname $SEARCHED_WINDOW)
-VISIBLE_W_ID=$(xdotool search --onlyvisible --classname $SEARCHED_WINDOW 2>/dev/null)
+SEARCHED_WINDOW=$1
+COMMAND=${2:-$SEARCHED_WINDOW}
+SEARCHED_WINDOW_CLASSNAME=toggleApp$SEARCHED_WINDOW
+WINDOW_ID=$(xdotool search --classname $SEARCHED_WINDOW_CLASSNAME)
+VISIBLE_WINDOW_ID=$(xdotool search --onlyvisible --classname $SEARCHED_WINDOW_CLASSNAME 2>/dev/null)
 
-if [ -z "$W_ID" ]; then
-	echo "NO WINDOW FOUNT"
-	$SEARCHED_WINDOW
-	NEW_W_ID=$(wmctrl -lx | grep -i $SEARCHED_WINDOW | head -n 1 | cut -d ' ' -f1)
-	xdotool windowfocus $NEW_W_ID
-elif [ -z "$VISIBLE_W_ID" ]; then
-	echo "UNMAPPED WINDOW FOUND"
-	for wid in $W_ID; do
-		if [[ !($(xprop -id $wid WM_WINDOW_ROLE) =~ .*not\ found.* )]]; then
-			xdotool windowmap $wid
-			xdotool windowfocus $wid
-		fi
-	done
+if [ -z "$WINDOW_ID" ]; then
+	$COMMAND 2>/dev/null &
+	pid=$!
+	NEW_WINDOW_ID=$(xdotool search --onlyvisible --sync --pid $pid 2>/dev/null)
+	xdotool set_window --classname $SEARCHED_WINDOW_CLASSNAME $NEW_WINDOW_ID
+	xdotool windowfocus $NEW_WINDOW_ID
+elif [ -z "$VISIBLE_WINDOW_ID" ]; then
+	xdotool windowmap $WINDOW_ID
+	xdotool windowfocus $WINDOW_ID
 else
-	echo "VISIBLE WINDOW FOUND"
-	windowPosition=$(xdotool getwindowgeometry $VISIBLE_W_ID | grep Position | sed "s/^.* \([0-9]*,[0-9]*\).*$/\1/")
-	windowX=$(echo $windowPosition | cut -d ',' -f1)
-	windowY=$(echo $windowPosition | cut -d ',' -f2)
-	echo $windowX $windowY
-
-	xdotool windowunmap --sync $VISIBLE_W_ID
-	sleep 0.2s
-	xdotool windowmove $VISIBLE_W_ID $windowX $windowY
+	xdotool windowunmap $VISIBLE_WINDOW_ID
 fi
