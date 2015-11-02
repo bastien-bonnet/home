6a3699e86635120f87e0cb67658835e1024cc892
bbonnet <none>
2015-11-02 16:58:24 +0100

    [scripts] Add option to workingDaysInMonth.bash: months to go back

--- .scripts/workingDaysInMonth.bash
+++ .scripts/workingDaysInMonth.bash
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 # This script prints a list of working days (monday to friday) dates in ISO-8601 format, date only (e.g., 2015-09-24)
-# Accepts one optional argument, which is the string that will be appended after each date in the output.
+# Accepts two optional arguments:
+#		- string that will be appended after each date in the output ;
+#		- the number of months to go back (i.e. "1" for last month).
 
 stringToAppendToDate="$1"
-currentYear="$(date +%Y)"
-currentMonth="$(date +%m)"
+monthsToGoBack="$2"
+
+fifteenthOfCurrentMonth=$(date +%Y-%m-15)
+monthsToGoBack="${2:-0}"
+currentYear=$(date -d "$fifteenthOfCurrentMonth - $monthsToGoBack month" +%Y)
+currentMonth=$(date -d "$fifteenthOfCurrentMonth - $monthsToGoBack month" +%m)
 lastDayOfMonth="$(date -d "$currentYear-$currentMonth-1 + 1 month - 1 day" "+%d")"
 TRUE="true"
 FALSE="false"
