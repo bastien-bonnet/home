1e76a0a8e462aa8da0b6b079b71c465a6b956890
bbonnet <none>
2020-01-18 15:20:50 +0100

    [conky] Refactor weather

--- .scripts/conky_weather.bash
+++ .scripts/conky_weather.bash
@@ -1,24 +1,26 @@
 #!/bin/bash
 
 main() {
+	local field_separator=","
 	local weather_data=$(\
 		get_weather_html_table \
-		| html_table_to_simple_table \
-		| select_relevant_columns)
+		| html_table_to_simple_table "$field_separator" \
+		| select_relevant_columns "$field_separator")
 
-	transpose_table "$weather_data"	| format
+	transpose_table "$weather_data"	"$field_separator" | format
 }
 
 get_weather_html_table() {
-	WEATHER_URL="https://weather.com/en-GB/weather/hourbyhour/l/b79a975f5ebe516146715560b2cb2e9de56e7b6e90e002c2bcc465c14ef65e22"
+	local WEATHER_URL="https://weather.com/en-GB/weather/hourbyhour/l/b79a975f5ebe516146715560b2cb2e9de56e7b6e90e002c2bcc465c14ef65e22"
 
 	local weather_html_page=$(curl $WEATHER_URL 2>/dev/null)
 	echo $weather_html_page | xmllint --html --xpath "//table[contains(@class, 'twc-table')]" --nowarning --noblanks - 2>/dev/null
 }
 
 html_table_to_simple_table() {
+	local field_separator="$1"
 	local replace_closing_trs_by_newlines='s/<\/tr>/\n/g'
-	local replace_opening_tds_by_separator="s/<td[^>]\+>/,/g"
+	local replace_opening_tds_by_separator="s/<td[^>]\+>/$field_separator/g"
 	local remove_all_html_tags='s/<[^>]\+>//g'
 	sed \
 		-e "$replace_closing_trs_by_newlines" \
@@ -27,24 +29,27 @@ html_table_to_simple_table() {
 }
 
 select_relevant_columns() {
-	awk -F, 'BEGIN { OFS = ","} NR>=2&&NR<=11{ print $3, $4, substr($5,1,length($5)-2)"/"substr($6,1,length($6)-2)"°", $7}; NR==12{ exit }'
+ local field_separator="$1"
+	awk -F "$field_separator" -v field_separator="$field_separator" 'BEGIN { OFS = field_separator } NR>=2&&NR<=11{ print $3, $4, substr($5,1,length($5)-2)"/"substr($6,1,length($6)-2)"°", $7}; NR==12{ exit }'
 }
 
 transpose_table() {
 local weather_data="$1"
+local field_separator="$2"
 for row in {1..4}; do
-		local transposed_line=$(echo "$weather_data" | cut -d ',' -f $row | paste -sd,)
-		local transposed_line_with_conky_offsets=$(replace_separator_with_conky_offset "$transposed_line")
+		local transposed_line=$(echo "$weather_data" | cut -d "$field_separator" -f $row | paste -sd "$field_separator")
+		local transposed_line_with_conky_offsets=$(replace_separator_with_conky_offset "$transposed_line" $field_separator)
 		echo $transposed_line_with_conky_offsets
 	done
 }
 
 replace_separator_with_conky_offset() {
 	local hours_to_show=9
+	local field_separator="$2"
 	local result="$1"
 	for field_number in $(seq $hours_to_show); do
 		local offset="$((50 + $field_number * 50))"
-		local result=$(echo "$result" | sed -e "s/,/\${goto $offset}/1")
+		local result=$(echo "$result" | sed -e "s/$field_separator/\${goto $offset}/1")
 	done
 	echo "$result"
 }
