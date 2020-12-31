0103100383bd39871f2c16f320736d323a1ab0b5
bbonnet <none>
2020-12-31 16:31:44 +0100

    [scripts] Add command to delete old kernels

--- .scripts/old_kernels.bash
+++ .scripts/old_kernels.bash
@@ -12,6 +12,17 @@ versions_to_keep=$(sort -u <<< "$running_version"$'\n'"$last_2_versions" )
 
 versions_to_delete=$(comm -23 <(echo "$found_versions") <(echo "$versions_to_keep"))
 
-pkgs_to_delete=$([[ -z "$versions_to_delete" ]] || grep -f <(echo "$versions_to_delete") <<< "$installed_kernel_pkgs")
+pkgs_to_delete=$([[ -n "$versions_to_delete" ]] && grep -f <(echo "$versions_to_delete") <<< "$installed_kernel_pkgs")
 
-echo "$pkgs_to_delete"
+echo "
+Current kernel version:
+$running_version
+
+Versions that shoud be kept (last 2 kernels):
+$versions_to_keep
+
+versions that can be deleted:
+$versions_to_delete
+
+Command to delete them:"
+echo sudo apt purge $pkgs_to_delete | grep --color=always "$VERSION_PATTERN"
