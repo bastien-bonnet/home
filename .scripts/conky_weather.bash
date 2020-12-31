e568da23333cdf3b1831f0f028fb43d492e89751
bbonnet <none>
2020-12-31 16:33:13 +0100

    [conky] Fix weather precipitation display

--- .scripts/conky_weather.bash
+++ .scripts/conky_weather.bash
@@ -74,7 +74,7 @@ replace_separator_with_conky_offset() {
 format() {
 	extract_hours='1s/\([[:digit:]]\{2\}:[[:digit:]]\{2\}\)[[:alpha:]]*/\1/g'
 	remove_0_precipitation='4s/-0%-//g'
-	enclose_precipitations='4s/\([[:digit:]]\{1,2\}%\)/-\1-/g'
+	enclose_precipitations='4s/\([[:digit:]]\{1,3\}%\)/-\1-/g'
 	format_precipitations='4s/-\([[:digit:]]\{1,3\}%\)-/\${font Font Awesome 5 Free Solid:style=Solid:size=9}\${font DejaVu Sans Mono:size=8}\1/g'
 	prepend_conky_indent='s/^/\${goto 70}/'
 	enclose_weather_icon_line_with_font_tags='2s/^\(.*\)$/\${font Font Awesome 5 Free Solid:style=Solid:size=18}\1\${font DejaVu Sans Mono:size=8}/'
