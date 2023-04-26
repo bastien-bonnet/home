d2a8a665f0eecb9c9a09a7a9b556b63165ae33ed
Bastien <none>
2023-04-26 11:26:52 +0200

    [scripts] Add snaps to freeable space

--- .scripts/easily_freeable_space.bash
+++ .scripts/easily_freeable_space.bash
@@ -2,3 +2,6 @@
 
 du -hs ~/.local/share/Trash/files
 sudo du -hs /tmp /var/tmp /var/cache
+
+echo -e "\nRemovable (disabled) snaps:"
+snap list --all | awk '/disabled/{print $1" --revision "$3}' | xargs -rn3 echo sudo snap remove
