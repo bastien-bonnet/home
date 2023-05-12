eec585f6b7a0fdb6e12a8b8064c588abafaeb403
Bastien <none>
2023-05-12 20:54:21 +0200

    [scripts] Freeable space: improve display

--- .scripts/easily_freeable_space.bash
+++ .scripts/easily_freeable_space.bash
@@ -6,5 +6,7 @@ sudo du -hs /tmp /var/tmp /var/cache
 echo -e "\nRemovable (disabled) snaps:"
 snap list --all | awk '/disabled/{print $1" --revision "$3}' | xargs -rn3 echo sudo snap remove
 
-echo -e "\nClear systemd journal to keep only last 2 days:"
+echo ""
+journalctl --disk-usage
+echo "Clear systemd journal to keep only last 2 days:"
 echo "sudo journalctl --vacuum-time=2d"
