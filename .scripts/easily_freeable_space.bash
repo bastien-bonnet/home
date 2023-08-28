e599cdfdb49af51bf065da443f7842f58c10907e
Bastien <none>
2023-08-28 11:16:29 +0200

    [scripts] Freeable space: add journalctl instructions

--- .scripts/easily_freeable_space.bash
+++ .scripts/easily_freeable_space.bash
@@ -8,5 +8,12 @@ snap list --all | awk '/disabled/{print $1" --revision "$3}' | xargs -rn3 echo s
 
 echo ""
 journalctl --disk-usage
-echo "Clear systemd journal to keep only last 2 days:"
-echo "sudo journalctl --vacuum-time=2d"
+echo "You can either :"
+echo "1. Clear systemd journal to keep only last 2 days:"
+echo "    sudo journalctl --vacuum-time=2d"
+echo "2. Schedule journald to only keep as much data as you are interested in by"
+echo "editing /etc/systemd/journald.conf and changing size or time settings:"
+echo "    MaxRetentionSec=2day"
+echo "    SystemMaxUse=100M"
+echo "Then restart daemon with"
+echo "    systemctl restart systemd-journald"
